8605ce6e3ddde869909051699247c460d841bcb6
cris03b <62027804+cris03b@users.noreply.github.com>
2020-03-16 18:58:38 +0200

    Add files via upload

---
+++ complex.h
@@ -0,0 +1,27 @@
+#pragma once
+class Complex {
+private:
+	double real;
+	double imaginar;
+	
+
+public:
+
+	Complex(double real, double imaginar);
+	Complex();
+	~Complex();
+	
+	double getReal() const;
+	void setReal(double real);
+	double getImaginary() const;
+	void setImaginary(double imaginar);
+	
+	
+	
+	Complex Add(Complex y);
+	Complex Mult(Complex x);
+	Complex Quot(Complex x);
+	double Abs();
+	double ComputePolar();
+
+};
