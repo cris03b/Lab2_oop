6aa71f9494958c5dc36743d8ba8f8b9eb86cb404
cris03b <62027804+cris03b@users.noreply.github.com>
2020-03-16 18:56:41 +0200

    Create complex.cpp

---
+++ complex.cpp
@@ -0,0 +1,63 @@
+#include "complex.h"
+#include <iostream>
+#include <cmath>
+
+
+Complex::Complex() {
+	real = 0;
+	imaginar = 0;
+}
+
+Complex::Complex(double real, double imaginar) {
+	this->real = real;
+	this->imaginar = imaginar;
+}
+
+Complex::~Complex() {
+
+}
+
+// getter
+double Complex::getReal() const {
+	return real;
+}
+
+// setter
+void Complex::setReal(double real) {
+	this->real = real;
+}
+
+// getter
+double Complex::getImaginary() const{
+	return imaginar;
+}
+
+// setter
+void Complex::setImaginary(double imaginar) {
+	this->imaginar = imaginar;
+}
+
+Complex Complex::Add(Complex y) {
+	Complex suma;
+	suma.real = real + y.real;
+	suma.imaginar = imaginar + y.imaginar;
+	return suma;
+}
+
+Complex Complex::Mult(Complex y) {
+	Complex rezultat;
+	rezultat.real = real * y.real - imaginar * y.imaginar;
+	rezultat.imaginar = real * y.imaginar + imaginar * y.real;
+	return rezultat;
+}
+
+Complex Complex::Quot(Complex y) {
+	Complex rezultat;
+	rezultat.real = (real * y.real + imaginar * y.imaginar) / (y.real * y.real + y.imaginar * y.imaginar);
+	rezultat.imaginar = (y.real * imaginar - real * y.imaginar) / (y.real * y.real + y.imaginar * y.imaginar);
+	return rezultat;
+}
+
+double Complex::Abs() {
+	return sqrt(real * real + imaginar * imaginar);
+}
