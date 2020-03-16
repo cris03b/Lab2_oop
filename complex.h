a955e4914595fbc45bda49bb85bce7eef68745e8
cris03b <62027804+cris03b@users.noreply.github.com>
2020-03-16 20:44:19 +0200

    Add files via upload

--- complex.h
+++ complex.h
@@ -17,11 +17,11 @@ public:
 	void setImaginary(double imaginar);
 	
 	
-	
+	void show_compl();
+	void show_exp();
 	Complex Add(Complex y);
 	Complex Mult(Complex x);
 	Complex Quot(Complex x);
 	double Abs();
 	double ComputePolar();
-
 };
